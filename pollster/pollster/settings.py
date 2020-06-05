83434af907a29e2ce92307724202ab06788c702f
kostanzhoglo <jeffreywithers@gmail.com>
2020-06-05 14:53:16 -0400

    Make polls migration and migrate them

--- pollster/pollster/settings.py
+++ pollster/pollster/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',       # adds /pollster/polls/apps.py/PollsConfig class
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
