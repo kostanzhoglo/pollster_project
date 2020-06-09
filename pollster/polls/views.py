d7ef9ddaa7b0c8fb722a4fe4bdde40cf09359abd
kostanzhoglo <jeffreywithers@gmail.com>
2020-06-09 12:25:36 -0400

    Create basic render function in views.py and create polls/urls.py

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 # import your models, otherwise you'll never know what to render in the 1st place
 from .models import Question, Choice
+
+# Get Questions and display them
+def index(request):
+    # this will return a basic template.
+    # 1st param == Passing in the request object, 
+    # 2nd param == location of the template. 
+    # This does NOT create the template itself OR the URL needed. More work to do!
+        # Inside polls folder, create a urls.py to make the URL.
+    return render(request, 'polls/index.html')
