2492f8478974ec2e137f35d71b9da6ec6605c8f2
kostanzhoglo <jeffreywithers@gmail.com>
2020-06-09 12:18:48 -0400

    Customize the admin title, header, index area in admin.py and import models to views.py

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
+# Change header in Admin pages. Default is "Django Administration"
+admin.site.site_header = "Pollster Admin"
+# This is the browser tab text
+admin.site.site_title = "Pollster Admin Area"
+# Default is "Site administration"
+admin.site.index_title = "Welcome to the Pollster admin area"
+
 # in admin, this [TABULAR INLINE] lets admin-user see Choice options underneath Question on Question page
 class ChoiceInline(admin.TabularInline):
     model = Choice
--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# import your models, otherwise you'll never know what to render in the 1st place
+from .models import Question, Choice
