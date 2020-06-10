fbd1943e5f211595f0871d70e8dd9dc21086403f
kostanzhoglo <jeffreywithers@gmail.com>
2020-06-10 13:40:03 -0400

    Add route for detail in urls.py

--- pollster/polls/urls.py
+++ pollster/polls/urls.py
@@ -7,5 +7,8 @@ app_name = 'polls'
 urlpatterns = [
     # the '' on the next line actually means 'polls/'.
     # if instead we put, 'result' it would be 'polls/result'
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:question_id>', views.detail, name='detail')
 ]
+
+
