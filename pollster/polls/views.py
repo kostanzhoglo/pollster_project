6b63b0eba1ebad5b4900acfd53cf4f78fbe32a5b
kostanzhoglo <jeffreywithers@gmail.com>
2020-06-10 12:22:53 -0400

    Pass in questions objects to views.py

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -7,9 +7,12 @@ from .models import Question, Choice
 
 # Get Questions and display them
 def index(request):
+    # This will pass in our Questions list to the index.html page, so that info will be displayed
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
     # this will return a basic template.
     # 1st param == Passing in the request object, 
     # 2nd param == location of the template. 
     # This does NOT create the template itself OR the URL needed. More work to do!
         # Inside polls folder, create a urls.py to make the URL.
-    return render(request, 'polls/index.html')
+    return render(request, 'polls/index.html', context)
