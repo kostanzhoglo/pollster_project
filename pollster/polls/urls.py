23f2a1f502439ee3f9a36e432a57b196d8206920
kostanzhoglo <jeffreywithers@gmail.com>
2020-06-10 13:46:26 -0400

    Add results function and url

--- pollster/polls/urls.py
+++ pollster/polls/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     # the '' on the next line actually means 'polls/'.
     # if instead we put, 'result' it would be 'polls/result'
     path('', views.index, name='index'),
-    path('<int:question_id>', views.detail, name='detail')
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results', views.results, name='results')
 ]
 
 
--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -24,3 +24,8 @@ def detail(request, question_id):
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, 'polls/results.html', { 'question': question })
+
+# Get question and display results
+def results(request, question_id):
+    question = get-object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', { 'question': question })
